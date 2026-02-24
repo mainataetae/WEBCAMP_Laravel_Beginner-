c640cfe4749eaaf83452c37187eff498ef4feccf
mainataetae <maina1216@icloud.com>
2026-02-24 14:35:53 +0000

    1Page目と2Page目を書いた

---
+++ todolist/app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    /**
+     * トップページを表示する
+     * 
+     * @return \Illuminate\View\View
+     */
+     public function index()
+    {
+        return view('welcome');
+    }
+
+    public function second()
+    {
+        return view('welcome_second');
+    }
+}
